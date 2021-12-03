b9497687e5c87e60797780bca5712bd1ec819276
vplauzon <vincent_lauzon@yahoo.com.au>
2021-12-03 11:00:20 -0500

    Add console project

---
+++ code/kusto-copy/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+// See https://aka.ms/new-console-template for more information
+
+namespace delta_kusto
+{
+    internal class Program
+    {
+        public static string AssemblyVersion
+        {
+            get
+            {
+                var versionAttribute = typeof(Program)
+                    .Assembly
+                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+                var version = versionAttribute == null
+                    ? "<VERSION MISSING>"
+                    : versionAttribute!.InformationalVersion;
+
+                return version;
+            }
+        }
+
+        internal static async Task<int> Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+            CultureInfo.CurrentUICulture = CultureInfo.GetCultureInfo("en-US");
+
+            Console.WriteLine();
+            Console.WriteLine($"kusto-copy { AssemblyVersion }");
+            Console.WriteLine();
+
+            return await Task.FromResult(0);
+       }
+    }
+}
