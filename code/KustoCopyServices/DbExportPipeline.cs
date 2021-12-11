1bd6b1dd5cc41bc2d14b9bae1d7bbea08c1619b4
vplauzon <vincent_lauzon@yahoo.com.au>
2021-12-11 11:40:59 -0500

    Use union query to limit amount of queries

--- code/KustoCopyServices/DbExportPipeline.cs
+++ code/KustoCopyServices/DbExportPipeline.cs
@@ -7,6 +7,8 @@ namespace KustoCopyServices
 {
     internal class DbExportPipeline
     {
+        private const int DEFAULT_FETCH_TABLES_SIZE = 100;
+
         private readonly DbExportBookmark _exportBookmark;
         private readonly KustoClient _kustoClient;
         private readonly ITempFolderService _tempFolderService;
@@ -61,29 +63,71 @@ namespace KustoCopyServices
                 r => (string)r["Cursor"]);
             var tableNames = await tableNamesTask;
             var latestCursor = cursors.First();
-            var tableBookmarkTasks = tableNames
-                .Select(t => kustoClient.SetParameter("Cursor", latestCursor).ExecuteQueryAsync(
-                    dbName,
-                    "declare query_parameters(Cursor:string);"
-                    + $"{t} | where cursor_before_or_at(Cursor) "
-                    + "| summarize Min = min(ingestion_time()), Max = max(ingestion_time())",
-                    r => new TableBookmark
-                    {
-                        TableName = t,
-                        IsBackfill = true,
-                        MinTime = r["Min"].To<DateTime>(),
-                        MaxTime = r["Max"].To<DateTime>(),
-                        ExportedUntilTime = r["Min"].To<DateTime>()
-                    }))
-                .ToImmutableArray();
-
-            await Task.WhenAll(tableBookmarkTasks);
-
-            var tableBookmarks = tableBookmarkTasks
-                .Select(t => t.Result.First())
-                .ToImmutableArray();
+            var tableBookmarks = await FetchTableBookmarksAsync(
+                dbName,
+                kustoClient,
+                latestCursor,
+                tableNames,
+                DEFAULT_FETCH_TABLES_SIZE);
 
             return (latestCursor, tableBookmarks);
+        }
+
+        private static async Task<ImmutableArray<TableBookmark>> FetchTableBookmarksAsync(
+            string dbName,
+            KustoClient kustoClient,
+            string latestCursor,
+            IImmutableList<string> tableNames,
+            int chunkSize)
+        {
+            var tableChunks = tableNames.Chunk(chunkSize);
+            var bookmarkTasks = tableChunks
+                .Select(c => FetchTableChunkBookmarksAsync(
+                    dbName,
+                    kustoClient,
+                    latestCursor,
+                    c.ToImmutableArray()))
+                .ToImmutableArray();
+
+            await Task.WhenAll(bookmarkTasks);
+
+            var bookmarks = bookmarkTasks
+                .SelectMany(t => t.Result)
+                .ToImmutableArray();
+
+            return bookmarks;
+        }
+
+        private static async Task<ImmutableArray<TableBookmark>> FetchTableChunkBookmarksAsync(
+            string dbName,
+            KustoClient kustoClient,
+            string latestCursor,
+            IImmutableList<string> tableNames)
+        {
+            var commandHeader = @"
+declare query_parameters(Cursor:string);
+let fetchRange = (tableName:string) {
+    table(tableName)
+    | where cursor_before_or_at(Cursor)
+    | summarize Min = min(ingestion_time()), Max = max(ingestion_time())
+    | extend TableName = tableName
+};
+";
+            var tableCommandlets = tableNames.Select(t => $"fetchRange('{t}')");
+            var commandText = commandHeader + string.Join(" | union ", tableCommandlets);
+            var bookmarks = await kustoClient.SetParameter("Cursor", latestCursor).ExecuteQueryAsync(
+                dbName,
+                commandText,
+                r => new TableBookmark
+                {
+                    TableName = (string)r["TableName"],
+                    IsBackfill = true,
+                    MinTime = r["Min"].To<DateTime>(),
+                    MaxTime = r["Max"].To<DateTime>(),
+                    ExportedUntilTime = r["Min"].To<DateTime>()
+                });
+
+            return bookmarks;
         }
     }
 }
