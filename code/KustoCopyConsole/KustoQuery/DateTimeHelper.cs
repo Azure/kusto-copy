b3490b8532d2fe2313060aef49b0197561576867
vplauzon <vincent_lauzon@yahoo.com.au>
2022-11-28 16:03:30 -0500

    Remove dedup of code

--- code/KustoCopyConsole/KustoQuery/DateTimeHelper.cs
+++ code/KustoCopyConsole/KustoQuery/DateTimeHelper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kusto.Data.Common;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +10,9 @@ namespace KustoCopyConsole.KustoQuery
 {
     public static class DateTimeHelper
     {
-        public static string ToKql(this DateTime d)
+        public static string ToKql(this DateTime value)
         {
-            var kql =
-                $"{d.Year:00}-{d.Month:00}-{d.Day:00}T{d.Hour:00}:{d.Minute:00}:{d.Second:00}.{d.Ticks % 10000000:0000000}Z";
+            var kql = CslDateTimeLiteral.AsCslString(value);
 
             return kql;
         }
