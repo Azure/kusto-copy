aa49bf7c6976de15df231002c4e012b590541f75
vplauzon <vincent_lauzon@yahoo.com.au>
2024-08-16 17:17:02 -0400

    .

---
+++ code/KustoCopyConsole/Kusto/Data/RecordDistribution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KustoCopyConsole.Kusto.Data
+{
+    internal record RecordDistribution(
+        string IngestionTimeStart,
+        string IngestionTimeEnd,
+        long Cardinality);
+}
