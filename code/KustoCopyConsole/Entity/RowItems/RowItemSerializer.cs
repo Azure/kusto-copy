e0e97d49da2dfeb0b43bdabf27c422b0c2e4bc86
vplauzon <vincent_lauzon@yahoo.com.au>
2024-11-08 16:36:44 -0500

    .

---
+++ code/KustoCopyConsole/Entity/RowItems/RowItemSerializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KustoCopyConsole.Entity.RowItems
+{
+    internal class RowItemSerializer
+    {
+        public void Serialize(RowItemBase item, TextWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RowItemBase Deserialize(TextReader reader)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
