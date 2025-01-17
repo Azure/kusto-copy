5a752b0564ccc86baed42d7130ead47856322f94
vplauzon <vincent_lauzon@yahoo.com.au>
2025-01-16 20:16:14 -0500

    .

--- code/KustoCopyConsole/Runner/AwaitExportedRunner.cs
+++ code/KustoCopyConsole/Runner/AwaitExportedRunner.cs
@@ -12,6 +12,10 @@ namespace KustoCopyConsole.Runner
 {
     internal class AwaitExportedRunner : RunnerBase
     {
+        #region Inner Types
+        private record ClusterBlocks(Uri ClusterUri, IEnumerable<BlockRowItem> BlockItems);
+        #endregion
+
         public AwaitExportedRunner(
            MainJobParameterization parameterization,
            RowItemGateway rowItemGateway,
@@ -26,9 +30,57 @@ namespace KustoCopyConsole.Runner
             CleanCompletingExports();
             while (!AllActivitiesCompleted())
             {
+                var clusterBlocks = GetClusterBlocks();
+                var tasks = clusterBlocks
+                    .Select(o => UpdateOperationsAsync(o.ClusterUri, o.BlockItems, ct))
+                    .ToImmutableArray();
+
+                await Task.WhenAll(tasks);
                 //  Sleep
                 await SleepAsync(ct);
             }
+        }
+
+        private IEnumerable<ClusterBlocks> GetClusterBlocks()
+        {
+            var exportingBlocks = RowItemGateway.InMemoryCache
+                .ActivityMap
+                .Values
+                .Where(a => a.RowItem.State != ActivityState.Completed)
+                .SelectMany(a => a.IterationMap.Values)
+                .Where(i => i.RowItem.State != IterationState.Completed)
+                .SelectMany(i => i.BlockMap.Values)
+                .Select(b => b.RowItem)
+                .Where(b => b.State == BlockState.Exporting);
+            var clusterBlocks = exportingBlocks
+                .Select(b => new
+                {
+                    RowItemGateway.InMemoryCache
+                    .ActivityMap[b.ActivityName]
+                    .RowItem
+                    .SourceTable
+                    .ClusterUri,
+                    BlockItem = b
+                })
+                .GroupBy(o => o.ClusterUri, o => o.BlockItem)
+                .Select(g => new ClusterBlocks(g.Key, g.ToImmutableArray()));
+            return clusterBlocks;
+        }
+
+        private async Task UpdateOperationsAsync(
+            Uri clusterUri,
+            IEnumerable<BlockRowItem> blockItems,
+            CancellationToken ct)
+        {
+            var dbClient = DbClientFactory.GetDbCommandClient(clusterUri, string.Empty);
+            var operationIdMap = blockItems
+                .ToImmutableDictionary(b => b.OperationId);
+            var status = await dbClient.ShowOperationsAsync(
+                KustoPriority.HighestPriority,
+                operationIdMap.Keys,
+                ct);
+
+            throw new NotImplementedException();
         }
 
         private void CleanCompletingExports()
