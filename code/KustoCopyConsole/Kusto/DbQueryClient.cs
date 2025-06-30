4aa00557a78f871f474e6d086ee41bedeb0d6688
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-30 18:34:18 -0400

    .

--- code/KustoCopyConsole/Kusto/DbQueryClient.cs
+++ code/KustoCopyConsole/Kusto/DbQueryClient.cs
@@ -144,7 +144,9 @@ BaseData
                 async () =>
                 {   //  Max interval is based on the string size of the extent IDs
                     //  They must all fit in a string for the .show command
-                    const int MAX_INTERVAL_COUNT = 25000;
+                    const int MAX_EXTENT_COUNT = 25000;
+                    //  Max interval we look into in one go
+                    const int MAX_INTERVAL_COUNT = 500000;
 
                     var dbUri = $"{_queryUri.ToString().TrimEnd('/')}/{_databaseName}";
                     var cursorStartFilter = string.IsNullOrWhiteSpace(cursorStart)
@@ -154,6 +156,7 @@ BaseData
                     ? string.Empty
                     : $@"| where ingestion_time()>todatetime('{lastIngestionTime}')";
                     var query = @$"
+let MaxExtentCount = {MAX_EXTENT_COUNT};
 let MaxIntervalCount = {MAX_INTERVAL_COUNT};
 let MaxRowCount = {maxRowCount};
 let TimeHorizon = 1d;
@@ -169,24 +172,28 @@ let BaseData = ['{tableName}']
 //  Do a first pass by merging ingestion time in the same extent id (regardless of row count)
 let ExtentsWithIngestionTimeIntervals = materialize(BaseData
     | summarize RowCount=count() by IngestionTime=ingestion_time(), ExtentId=extent_id()
-    //  We top number of intervals so the list of extent ids can fit in a 1MB string
-    | top {MAX_INTERVAL_COUNT} by IngestionTime asc
-    //  We keep the interval number so that if it matches MaxIntervals, corresponding ingestion time end should be dropped
-    | extend IntervalNumber=row_number(0)
+    //  We top number of intervals so the data set is workable in memory
+    | top MaxIntervalCount by IngestionTime asc
+    //  We keep the interval number so that if it matches MaxIntervalCount, corresponding ingestion time end should be dropped
+    | extend IntervalNumber=row_number(1)
     | extend Rank=row_rank_dense(ExtentId)
     | summarize IngestionTimeStart=min(IngestionTime), IngestionTimeEnd=max(IngestionTime),
         ExtentId=take_any(ExtentId), RowCount=sum(RowCount),
         MaxIntervalNumber=max(IntervalNumber)
         by Rank
-    //  We clip the MAX_INTERVAL_COUNT's interval as it may contain only parts of the
+    //  We clip the MaxIntervalCount's interval as it may contain only parts of the
     //  data pertaining to its ingestion_time
-    | where MaxIntervalNumber!={MAX_INTERVAL_COUNT}
+    | where MaxIntervalNumber!=MaxIntervalCount
     | project-away Rank, MaxIntervalNumber
     | extend ExtentId=tostring(ExtentId));
-//  Build a .show command to get the extent id creation time
-let ExtentIds = ExtentsWithIngestionTimeIntervals
-    | distinct ExtentId
+//  Compile the unique extent IDs with the intent of clipping them to MaxExtentCount
+let ExtentIdWithIngestionTime = ExtentsWithIngestionTimeIntervals
+    | summarize IngestionTimeStart=min(IngestionTimeStart) by ExtentId
+    //  Clip the extent ids:  this will give us an IngestionTimeStart to clip from
+    | top MaxExtentCount by IngestionTimeStart asc;
+let ExtentIds = ExtentIdWithIngestionTime
     | summarize make_list(ExtentId);
+//  Build a .show command to get the extent id creation time
 let ShowCommand = strcat(
     "".show table ['{tableName}'] extents ("",
     strcat_array(toscalar(ExtentIds), ','),
@@ -198,6 +205,8 @@ let ExtentIdCreationTime = evaluate execute_show_command(""{dbUri}"", ShowComman
 //  Join the two information and merge intervals for creation time within the same hour
 //  (while keeping row count under a threshold)
 ExtentsWithIngestionTimeIntervals
+//  Clip the data to align with extent IDs
+| where IngestionTimeStart <= toscalar(ExtentIdWithIngestionTime | summarize max(IngestionTimeStart))
 | lookup kind=leftouter ExtentIdCreationTime on ExtentId
 | extend GroupCreatedOn=bin(CreatedOn, 1d)
 | order by IngestionTimeStart asc, IngestionTimeEnd asc
