77c55c6b306a500d0814ba4c93458538764f3003
vplauzon <vincent_lauzon@yahoo.com.au>
2026-02-04 18:37:40 -0500

    .

---
+++ code/KustoCopyConsole/Entity/PlanningPartitionRecord.cs
@@ -0,0 +1,24 @@
+﻿using KustoCopyConsole.Entity.Keys;
+using KustoCopyConsole.Entity.State;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KustoCopyConsole.Entity
+{
+    internal record PlanningPartitionRecord(
+        BlockKey BlockKey,
+        int Generation,
+        bool IsLeftExplored,
+        long RecordCount,
+        string MinIngestionTime,
+        string MedianIngestionTime,
+        string MaxIngestionTime) : RecordBase
+    {
+        public override void Validate()
+        {
+        }
+    }
+}
