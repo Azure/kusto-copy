fc1a9ad4dcdae2db2940e9ecbfe157a2a4059acd
vplauzon <vincent_lauzon@yahoo.com.au>
2026-02-18 18:24:31 -0500

    .

--- code/KustoCopyConsole/Runner/PlanningRunner.cs
+++ code/KustoCopyConsole/Runner/PlanningRunner.cs
@@ -20,8 +20,8 @@ namespace KustoCopyConsole.Runner
             long RecordCount);
         #endregion
 
-        private const int MAX_ACTIVE_BLOCKS_PER_ITERATION = 1500;
-        private const int MIN_ACTIVE_BLOCKS_PER_ITERATION = 700;
+        private const int MAX_ACTIVE_BLOCKS_PER_ITERATION = 2000;
+        private const int MIN_ACTIVE_BLOCKS_PER_ITERATION = 1000;
         private const int MAX_ROW_COUNT_PER_BLOCK = 4000000;
         private const int MAX_ROW_COUNT_PER_PARTITION = 250 * MAX_ROW_COUNT_PER_BLOCK;
 
